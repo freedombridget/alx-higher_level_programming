ca0eeafc78086c5042ae84ced1df0b3ca475bb49
freedombridget <86961410+freedombridget@users.noreply.github.com>
2022-11-30 19:04:47 +0100

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
+// Concat two files
+
 const fs = require('fs');
-const textA = fs.readFileSync(process.argv[2], 'utf-8');
-const textB = fs.readFileSync(process.argv[3], 'utf-8');
-fs.writeFileSync(process.argv[4], textA + textB, 'utf-8');
+
+const data1 = fs.readFileSync(process.argv[2], 'utf8');
+const data2 = fs.readFileSync(process.argv[3], 'utf8');
+
+fs.writeFileSync(process.argv[4], data1 + data2);
