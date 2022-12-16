3cb64073f8c7e9d99b5c5c1314555d9dbdc369de
freedombridget <86961410+freedombridget@users.noreply.github.com>
2022-12-16 18:22:15 +0100

    0x11-python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+"""Script that fetches https://intranet.hbtn.io/status"""
 import requests
-
-if __name__ == '__main__':
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(
-          type(r.text), r.text))
+if __name__ == "__main__":
+    page = requests.get('https://intranet.hbtn.io/status')
+    page_content = page.text
+    print("Body response:")
+    print("\t- type: {}".format(type(page_content)))
+    print("\t- content: {}".format(page_content))
